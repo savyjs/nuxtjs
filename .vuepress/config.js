aa3f9e1325818bcd6e2689762e03cecb0b1d86a9
afshary <savvyversa@gmail.com>
2020-05-15 20:29:31 +0430

    add intro page - make template RTL

---
+++ .vuepress/config.js
@@ -0,0 +1,54 @@
+module.exports = {
+    themeConfig: {
+        smoothScroll: true,
+        logo: '/icon.png',
+        lastUpdated: 'بهار۹۹',
+        nextLinks: true,
+        // default value is true. Set it to false to hide prev page links on all pages
+        prevLinks: true,
+        repo: 'savyir/nuxtjs',
+        repoLabel: 'ریپوی ما!',
+        editLinks: true,
+        // custom text for edit link. Defaults to "Edit this page"
+        editLinkText: 'Help us improve this page!',
+        searchPlaceholder: 'جست و جو...',
+        nav: [
+            {text: 'صفحه نخست', link: '/'},
+            {text: 'مستندات راهنما', link: '/guide/'},
+            {text: 'مثال ها', link: '/examples/'},
+            {text: 'قالب ها', link: 'http://vip.nuxt-js.ir/templates'},
+//            {text: 'پشتیبانی', link: 'http://vip.nuxt-js.ir/ask'},
+
+            {
+                text: 'مراجع کمکی',
+                ariaLabel: 'سایت های اصلی مرجع',
+                items: [
+                    {text: 'ناکس جهانی', link: 'https://nuxtjs.org', target: '_blank'},
+                    {text: 'ویو', link: 'https://vuejs.org', target: '_blank'},
+                    {text: 'ویوتیفای', link: 'https://vuetifyjs.com', target: '_blank'},
+                ]
+            },
+            {text: 'ساوی', link: 'https://savy.ir', target: '_blank'},
+        ]
+    },
+    sidebarDepth: 4,
+    displayAllHeaders: true,
+    sidebar: [
+        {
+            title: 'مقدمه و شروع',   // required
+            path: '/guide',      // optional, link of the title, which should be an absolute path and must exist
+            collapsable: true, // optional, defaults to true
+            sidebarDepth: 4,    // optional, defaults to 1
+
+        },
+        {
+            title: 'مفاهیم کلی',   // required
+            path: '/guide',      // optional, link of the title, which should be an absolute path and must exist
+            collapsable: true, // optional, defaults to true
+            sidebarDepth: 4,    // optional, defaults to 1
+            children: [
+                '/'
+            ]
+        },
+    ],
+}
